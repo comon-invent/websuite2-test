8a4ed154f7f12bf9ba6aceeb0b7b9f46022003a3
ralphvn <142317939+ralphvn@users.noreply.github.com>
2023-08-30 12:19:34 +0000

    Added Vercel statistics to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import "../styles/tailwind.css"
 import "../styles/custom.css"
-
+import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
